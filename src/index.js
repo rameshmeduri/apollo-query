3c7a1f788e3da79c3540997c9399c4a05d4dc537
Ramesh Meduri <rameshmeduri@outlook.com>
2020-04-17 00:59:29 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
